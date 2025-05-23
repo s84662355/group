4b395f9b1d0755142452d4594c50b792bf096502
chenjiahao <271375247@qq.com>
2025-05-24 02:16:35 +0800

    a

--- group.go
+++ group.go
@@ -1,15 +1,11 @@
 package group
- 
 
 import (
 	"context"
 	"fmt"
 	"sync"
 	"sync/atomic"
-	
 )
-
- 
 
 type (
 	A[T any] func(ctx context.Context) (T, bool)
@@ -18,22 +14,22 @@ type (
 
 type FirstResultGroup[T any] struct {
 	has    atomic.Bool
-	c      atomic.Int32
 	result T
+	err    error
 	ctx    context.Context
 	cancel context.CancelFunc
 	wg     sync.WaitGroup
+	doOne  sync.Once
 }
 
-func NewFirstResultGroup[T any]() *FirstResultGroup[T]{
-   f:=&FirstResultGroup[T]{}
-   f.ctx,f.cancel = context. WithCancel(context.Background())  
+func NewFirstResultGroup[T any]() *FirstResultGroup[T] {
+	f := &FirstResultGroup[T]{}
+	f.ctx, f.cancel = context.WithCancel(context.Background())
 
-   return f
+	return f
 }
 
 func (f *FirstResultGroup[T]) Go(a A[T], b B[T]) {
-	f.c.Add(1)
 	f.wg.Add(1)
 	go func() {
 		defer f.wg.Done()
@@ -46,34 +42,40 @@ func (f *FirstResultGroup[T]) Go(a A[T], b B[T]) {
 				b(r)
 			}
 		}
-		if f.c.Add(-1) == 0 {
-			f.cancel()
-		}
 	}()
 }
 
 func (f *FirstResultGroup[T]) GetResult(done <-chan struct{}) (T, error) {
-	defer f.wg.Wait()
-	select {
-	case <-f.ctx.Done():
-		if !f.has.CompareAndSwap(false, true) {
-			return f.result, nil
-		} else {
-			return f.result, fmt.Errorf("任务失败")
+	f.doOne.Do(func() {
+		d := make(chan struct{})
+		defer func() {
+			for range d {
+				/* code */
+			}
+		}()
+		go func() {
+			defer close(d)
+			f.wg.Wait()
+			f.cancel()
+		}()
+		select {
+		case <-f.ctx.Done():
+			if !f.has.CompareAndSwap(false, true) {
+				return
+			} else {
+				f.err = fmt.Errorf("任务失败")
+				return
+			}
+
+		case <-done:
+			f.cancel()
+			if f.has.CompareAndSwap(false, true) {
+				f.err = fmt.Errorf("自定义上下结束")
+				return
+			}
+
 		}
-
-	case <-done:
-		f.cancel()
-		if f.has.CompareAndSwap(false, true) {
-			return f.result, fmt.Errorf("自定义上下结束")
-		}
-
-	}
-
+	})
 	<-f.ctx.Done()
-	return f.result, nil
+	return f.result, f.err
 }
-
- 
- 
- 
